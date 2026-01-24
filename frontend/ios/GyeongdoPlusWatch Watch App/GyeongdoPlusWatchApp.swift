269b7124b423cd57f12663415d8ac02e1db30eec
Junyeop Lee <orca@sparcs.org>
2026-01-24 23:20:42 +0900

    chore: connect success with watch and iOS

---
+++ frontend/ios/GyeongdoPlusWatch Watch App/GyeongdoPlusWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyeongdoPlusWatchApp.swift
+//  GyeongdoPlusWatch Watch App
+//
+//  Created by 이준엽 on 1/24/26.
+//
+
+import SwiftUI
+
+@main
+struct GyeongdoPlusWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/ios/GyeongdoPlusWatch Watch AppTests/GyeongdoPlusWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GyeongdoPlusWatch_Watch_AppTests.swift
+//  GyeongdoPlusWatch Watch AppTests
+//
+//  Created by 이준엽 on 1/24/26.
+//
+
+import Testing
+@testable import GyeongdoPlusWatch_Watch_App
+
+struct GyeongdoPlusWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
